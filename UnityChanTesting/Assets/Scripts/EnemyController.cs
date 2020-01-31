2ed8ef7672adc9e12a00d041f00a5a9bfdcd7cac
bacchusjazz <bacchusjazz6248@gmail.com>
2020-01-31 14:28:18 +0900

    範囲内侵入による敵追跡スクリプト追加

---
+++ UnityChanTesting/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class EnemyController : MonoBehaviour
+{
+    [System.NonSerialized]
+    public bool isActive = false;
+    [SerializeField]
+    NavMeshAgent agent;
+    BoxCollider bcollider;
+    public GameObject target;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        bcollider = GameObject.Find("RIG").GetComponent<BoxCollider>();
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+    }
+
+    void Update()
+    {
+        OnDetectObject(bcollider);
+    }
+
+    public void OnDetectObject(Collider collider)
+    {
+        if (isActive)
+        {
+            transform.LookAt(target.transform.position);
+            agent.destination = target.transform.position;
+        }
+    }
+
+    //検出範囲に入った時の処理
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+
+    //検出範囲から出たときの処理
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+}
